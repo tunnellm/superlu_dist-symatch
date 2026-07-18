841fafa5cdd0d4b940b68970582fe6914807af09
Marc <mtunnell@purdue.edu>
2026-07-18 01:40:15 -0400

    Preserve index width in CUDA dispatch bridges

--- SRC/CplusplusFactor/LUgpuCHandle_interface_impl.cu
+++ SRC/CplusplusFactor/LUgpuCHandle_interface_impl.cu
@@ -14,7 +14,7 @@ extern "C"
     int pdgstrf3d_symv2_factor_cpp(dLUgpu_Handle handle);
     void pdgstrf3d_symv2_destroy_cpu_runtime_cpp(dLUgpu_Handle handle);
 
-    int pdgstrf3d_symv2_diag_panel_cuda_bridge(
+    int_t pdgstrf3d_symv2_diag_panel_cuda_bridge(
         void *handle, int_t k, int_t handle_offset, int_t buffer_offset,
         ddiagFactBufs_t **diag_buffers)
     {
@@ -24,7 +24,7 @@ extern "C"
             k, handle_offset, buffer_offset, diag_buffers);
     }
 
-    int pdgstrf3d_symv2_factor_forest_cuda_bridge(
+    int_t pdgstrf3d_symv2_factor_forest_cuda_bridge(
         void *handle, sForest_t *forest, ddiagFactBufs_t **diag_buffers,
         gEtreeInfo_t *etree, int tag_ub)
     {
@@ -34,7 +34,7 @@ extern "C"
             forest, diag_buffers, etree, tag_ub);
     }
 
-    int pdgstrf3d_symv2_ancestor_cuda_bridge(
+    int_t pdgstrf3d_symv2_ancestor_cuda_bridge(
         void *handle, int_t level, int_t *node_count, int_t **tree_perm)
     {
         xLUstruct_t<double> *lu =
