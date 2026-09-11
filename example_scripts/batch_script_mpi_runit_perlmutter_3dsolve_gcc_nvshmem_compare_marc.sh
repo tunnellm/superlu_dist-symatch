a870edc2bcd7bc89c3937aeeb87c7cd163ba33df
Yang Liu <liuyangzhuan@gmail.com>
2026-09-10 20:35:12 -0700

    update runscript

--- example_scripts/batch_script_mpi_runit_perlmutter_3dsolve_gcc_nvshmem_compare_marc.sh
+++ example_scripts/batch_script_mpi_runit_perlmutter_3dsolve_gcc_nvshmem_compare_marc.sh
@@ -17,7 +17,7 @@ ulimit -s unlimited
 # Set gpu=1 for GPU nodes/factorizations or gpu=0 for CPU
 # nodes/factorizations. An exported value overrides the default.
 
-gpu=0
+gpu=1
 gpu=${gpu:-1}
 if [[ $gpu != 0 && $gpu != 1 ]]; then
   echo "gpu must be either 0 or 1" >&2
@@ -231,18 +231,22 @@ else
   exit $EXIT_HOST
 fi
 
-mats=(symmetric/Geo_1438.bin symmetric/StocF-1465.bin symmetric/nlpkkt80.bin symmetric/dielFilterV3real.mtx symmetric/Si41Ge41H72.mtx symmetric/pwtk.mtx symmetric/offshore.mtx symmetric/Spielman_k200_A_10.mtx symmetric/Cube_Coup_dt0.mtx)
 
 
 
 if ((gpu)); then
+  export SUPERLU_GPU_MEMORY_PROFILE=1
   factor_modes=(gpu_ldlt gpu_old_lu gpu_symmetric_lu)
+
+  mats=(symmetric/Geo_1438.bin symmetric/StocF-1465.bin symmetric/nlpkkt80.bin symmetric/dielFilterV3real.mtx symmetric/Si41Ge41H72.mtx symmetric/pwtk.mtx symmetric/offshore.mtx symmetric/Spielman_k200_A_10.mtx symmetric/Cube_Coup_dt0.mtx)  
   nprows=(2 2 2 1 4 1 2 1 2)
   npcols=(1 1 1 2 1 2 1 2 1)
   npz=(8 8 8 8 4 8 8 8 8)  
   NTH=16
 else
   factor_modes=(cpu_ldlt cpu_old_lu cpu_symmetric_lu)
+
+  mats=(symmetric/Geo_1438.bin symmetric/StocF-1465.bin symmetric/nlpkkt80.bin symmetric/dielFilterV3real.mtx symmetric/Si41Ge41H72.mtx symmetric/pwtk.mtx symmetric/offshore.mtx symmetric/Spielman_k200_A_10.mtx symmetric/Cube_Coup_dt0.mtx)
   nprows=(8 2 8 2 4 2 4 1 8)
   npcols=(2 1 1 1 4 1 1 4 4)
   npz=(2 16 4 16 2 16 8 8 1)  
